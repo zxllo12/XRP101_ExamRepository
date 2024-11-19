b5548745641f607127ce08c4a261a949054a76c8
zxllo12 <poapog123@naver.com>
2024-11-19 11:46:00 +0900

    01번 문제 제출

--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -11,12 +11,14 @@ public class CubeManager : MonoBehaviour
 
     private void Awake()
     {
-        SetCubePosition(3, 0, 3);
+
     }
 
     private void Start()
     {
         CreateCube();
+
+        SetCubePosition(3, 0, 3); // 초기화가 되기전에 포지션을 설정해서 어웨이크에서 스타트로 내림
     }
 
     private void SetCubePosition(float x, float y, float z)
@@ -24,6 +26,7 @@ public class CubeManager : MonoBehaviour
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
+        _cubeController.SetPoint = _cubeSetPoint;
         _cubeController.SetPosition();
     }
 
@@ -31,6 +34,6 @@ public class CubeManager : MonoBehaviour
     {
         GameObject cube = Instantiate(_cubePrefab);
         _cubeController = cube.GetComponent<CubeController>();
-        _cubeSetPoint = _cubeController.SetPoint;
+        //_cubeSetPoint = _cubeController.SetPoint; // SetCubePosition이 나중에 호출되니 주석처리
     }
 }
